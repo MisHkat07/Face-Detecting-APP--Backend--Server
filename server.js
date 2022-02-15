b637ba9aa77af469601d31d42266edd18bd14236
MisHkat <mishkat606@gmail.com>
2022-02-15 23:02:07 +0600

    Heroku Port Update

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.put('/image',  (req, res) => { image.handleImage(req, res, db);});
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db);});
 
-app.listen(3000, () => {
-    console.log('app is running');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on ${process.env.PORT}`);
 });
