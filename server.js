fcf52bf8cc0e528b85d18eb77830c4aaffdc808f
MisHkat <mishkat606@gmail.com>
2022-04-28 20:00:51 +0600

    m

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const image = require("./Controllers/image");
 const db = knex({
   client: 'pg',
    connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '',
-    database : 'smart-brain'
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+      rejectUnauthorized: false
+  }
   }
 });
 
