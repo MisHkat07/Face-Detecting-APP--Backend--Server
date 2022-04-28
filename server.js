c8838c5234dc39487d53d28790d05f109eb20826
MisHkat <mishkat606@gmail.com>
2022-04-28 08:42:44 +0600

    m

--- server.js
+++ server.js
@@ -25,23 +25,14 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send("it is working");
-});
-app.post('/signin', signin.handleSignin(db, bcrypt));
-
-app.post('/register', (req, res) => {
-  register.handleRegister(req, res, db, bcrypt);
-});
-app.get('/profile/:id', (req, res) => {
-  profile.handleProfileGet(req, res, db);
-});
-app.put('/image', (req, res) => {
-  image.handleImage(req, res, db);
-});
-app.post('/imageurl', (req, res) => {
-  image.handleApiCall(req, res);
+    res.send("Success");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`);
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt);});
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt);});
+app.put('/image',  (req, res) => { image.handleImage(req, res, db);});
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db);});
+
+app.listen(3000, () => {
+    console.log('app is running');
 });
