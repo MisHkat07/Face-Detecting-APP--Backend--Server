6a043e978edff68da888aaa2f80368efc96003e6
MisHkat <mishkat606@gmail.com>
2022-04-28 10:38:56 +0600

    m

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ app.use(cors());
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+    connectingString : process.env.DATABASE_URL,
+    ssl : true
   }
 });
 
