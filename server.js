ee84a3e525c482ea640649777bfe33669f311921
MisHkat <mishkat606@gmail.com>
2022-04-28 17:36:57 +0600

    m

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.put('/image',  (req, res) => { image.handleImage(req, res, db);});
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db);});
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || '3000', () => {
     console.log('app is running');
 });
