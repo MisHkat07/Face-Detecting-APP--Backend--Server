e2e28c292509c2e0824bf20837bb6741265f3f3f
MisHkat <mishkat606@gmail.com>
2022-04-28 20:24:46 +0600

    m

--- server.js
+++ server.js
@@ -34,9 +34,6 @@ app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt);})
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt);});
 app.put('/image',  (req, res) => { image.handleImage(req, res, db);});
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db); });
-app.post('/imageurl', (req, res) => {
-    image.handleApi(req, res);
-});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
