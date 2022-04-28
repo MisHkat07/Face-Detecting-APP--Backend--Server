e6151cc3729629c27633403f7946be250206b9c5
MisHkat <mishkat606@gmail.com>
2022-04-28 21:17:24 +0600

    m

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const signin = require("./Controllers/signin");
 const profile = require("./Controllers/profile");
 const image = require("./Controllers/image");
 
-
 const db = knex({
   client: 'pg',
    connection: {
@@ -23,14 +22,14 @@ const db = knex({
 app.use(cors())
 app.use(express.json());
 
-
-app.get('/', (req, res)=> {
-  res.send(database.users);
-})
+app.get('/', (req, res)=> {res.send(database.users);})
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt);});
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt);});
 app.put('/image', (req, res) => { image.handleImage(req, res, db);});
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db); });
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
